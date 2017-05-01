55dc2f2155f7ec7b993bda519ea46f990ae256a8
Ryan <rymanske@gmail.com>
2017-05-01 01:48:25 -0400

    threaded file reading - not working yet

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <functional>
 #include <cctype>
 #include <locale>
+#include <dirent.h>
+#include <sys/types.h>
 
 #include <mpi.h>
 
@@ -16,13 +18,29 @@
 #define FILENUM 30
 #define NUM_DIRECTORIES 1296
 
-#define THREADS_PER_RANK 2
+#define THREADS_PER_RANK 1
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+
+struct thread_arg_t {
+  int threadLowerbound;
+  int threadUpperbound;
+
+  std::vector<std::string> *filePaths;
+};
 
 // MPI Variables
 int rank, num_procs;
 
 // int articles_per_rank;
 int directories_per_rank;
+int directories_per_thread;
+
+
 
 // Function Templates
 std::string getArticleFilename(int input);
@@ -31,7 +49,7 @@ std::string &ltrim(std::string &s);
 std::string &rtrim(std::string &s);
 std::string &trim(std::string &s);
 
-void *read_files(void *thread_arg);
+void *readFiles(void *thread_arg);
 
 int main(int argc, char *argv[]) {
 
@@ -51,21 +69,40 @@ int main(int argc, char *argv[]) {
   //   articles_per_rank = FILENUM / num_procs;
   // }
 
+
+  // indeces of first directory for this rank and first directory for next rank
+  int rankLowerbound = (rank * (NUM_DIRECTORIES / num_procs));
+  // int rankUpperbound = ((rank + 1) * NUM_DIRECTORIES / num_procs);
+
   // should divide evenly
   directories_per_rank = NUM_DIRECTORIES / num_procs;
+
+  directories_per_thread = directories_per_rank / THREADS_PER_RANK;
 
   // hold each of the thread ids
   pthread_t threads[THREADS_PER_RANK];
 
-  // dummy thread arg
-  void *thread_arg;
+  // store all the files for this rank here
+  std::vector<std::string> filePaths;
+
+  std::vector<thread_arg_t> thread_args;
+  for (int i = 0; i < THREADS_PER_RANK; i++) {
+
+    // divide up directories by thread
+    thread_arg_t tmp;
+    tmp.threadLowerbound = rankLowerbound + (directories_per_thread * i);
+    tmp.threadUpperbound = rankLowerbound + (directories_per_thread * (i + 1));
+    tmp.filePaths = &filePaths; // share this vector accross all threads
+
+    thread_args.push_back(tmp);
+  }
 
   // create threads
   for (int i = 0; i < THREADS_PER_RANK; i++) {
-    int rc = pthread_create(&threads[i], NULL, read_files, &thread_arg);
+    int rc = pthread_create(&threads[i], NULL, readFiles, &thread_args[i]);
 
     // if we need to pass arguments into the threads, use this line
-    // int rc = pthread_create(&threads[i], NULL, read_files, &thread_args[i]);
+    // int rc = pthread_create(&threads[i], NULL, readFiles, &thread_args[i]);
 
     if (rc != 0) {
       std::cerr << "MAIN: Could not create thread" << std::endl;
@@ -84,6 +121,8 @@ int main(int argc, char *argv[]) {
     delete(x);
   }
 
+  MPI_Barrier(MPI_COMM_WORLD);
+
   // Exit Program
   MPI_Finalize();
   return EXIT_SUCCESS;
@@ -98,7 +137,7 @@ std::string getArticleFilename(int input) {
 // takes in an integer that represents the number directory we want
 // returns a string, the two letter name of that directory
 std::string getDirectoryName(int input) {
-  std::string directoryName = "~~";
+  std::string directoryName = "~~/";
 
   char firstLetter, secondLetter;
 
@@ -144,29 +183,98 @@ std::string &trim(std::string &s) {
     return ltrim(rtrim(s));
 }
 
-void *read_files(void *thread_arg) {
-  // Multifile Read
-  int lowerbound = (rank * (FILENUM / num_procs));
-  int upperbound = (rank + 1) * FILENUM / num_procs;
-  for(int i = lowerbound; i < upperbound; i++) {
-    std::ifstream file(getArticleFilename(i).c_str());
-    if(file.is_open()) {
-      std::string line;
-      while(getline(file, line)) {
-        if(line.find("<title>") != std::string::npos) {
-          line = trim(line);
-          std::cout << line.substr(7, line.length() - 15) << std::endl;
-        }
-        if(line.find("<text>") != std::string::npos) {
-          // First line...
-          while(getline(file, line)) {
-            if(line.find("</text>") != std::string::npos) { break; }
+void *readFiles(void *arg) {
+
+  // int lowerbound = (rank * (FILENUM / num_procs));
+  // int upperbound = (rank + 1) * FILENUM / num_procs;
+
+  // // indeces of first directory for this rank and first directory for next rank
+  // int lowerbound = (rank * (NUM_DIRECTORIES / num_procs));
+  // int upperbound = ((rank + 1) * NUM_DIRECTORIES / num_procs);
+
+  thread_arg_t thread_args = *(thread_arg_t*)arg;
+
+  int threadLowerbound = thread_args.threadLowerbound;
+  int threadUpperbound = thread_args.threadUpperbound;
+  // *(thread_args.filePaths).xyz()
+
+  // for each directory in article/
+  for (int i = threadLowerbound; i < threadUpperbound; i++) {
+    std::string dirPath = "article/";
+
+    dirPath.append(getDirectoryName(i));
+
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir (dirPath.c_str())) != NULL) {
+      /* print all the files and directories within directory */
+      while ((ent = readdir (dir)) != NULL) {
+        // printf ("%s\n", ent->d_name);
+
+        // exclude hidden files
+        if (ent->d_name[0] == '.') { continue; }
+
+        pthread_mutex_lock(&mutex1);
+        // std::cout << dirPath + ent->d_name << std::endl;
+        // ent->d_name is the name of the file
+        (*thread_args.filePaths).push_back(dirPath + ent->d_name);
+        pthread_mutex_unlock(&mutex1);
+
+      }
+      closedir (dir);
+    } else {
+      /* could not open directory */
+      perror ("Couldn't open directory! ");
+      exit (EXIT_FAILURE);
+    }
+
+    // now that the files are stored in the vector, read them
+    pthread_mutex_lock(&mutex2);
+    int fileCount = (*thread_args.filePaths).size();
+    pthread_mutex_unlock(&mutex2);
+
+    // while files remain
+    while (fileCount > 0) {
+      pthread_mutex_lock(&mutex2);
+      if (fileCount == 0) { break; }
+      // std::cout << "rank " << rank << " file count " << fileCount << '\n';
+
+      std::string tmpPath = (*thread_args.filePaths).back();
+      (*thread_args.filePaths).pop_back();
+      fileCount = (*thread_args.filePaths).size();
+
+      pthread_mutex_unlock(&mutex2);
+
+
+      std::ifstream file(tmpPath.c_str());
+      if(file.is_open()) {
+        std::string line;
+        while(getline(file, line)) {
+
+          std::cout << line << '\n';
+          if(line.find("<title>") != std::string::npos) {
+            line = trim(line);
+
+            pthread_mutex_lock(&mutex2);
+            std::cout << line.substr(7, line.length() - 15) << std::endl;
+            pthread_mutex_unlock(&mutex2);
+
+          }
+          if(line.find("<text>") != std::string::npos) {
+            // First line...
+            while(getline(file, line)) {
+              if(line.find("</text>") != std::string::npos) { break; }
+            }
           }
         }
+      file.close();
       }
-    file.close();
     }
+
+
+
   }
+
 
   unsigned int *return_val = new unsigned int;
 
